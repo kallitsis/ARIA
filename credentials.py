fb0f37dac7d5ee532fba596b2dd44be8767fc878
Evangelos Kallitsis <160036962+kallitsis@users.noreply.github.com>
2025-03-17 16:27:54 +0000

    Delete credentials.py

--- credentials.py
+++
@@ -1,7 +0,0 @@
-# credentials.py
-# This file contains confidential credentials.
-# DO NOT push this file to GitHub.
-
-OPENAI_API_KEY = "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"
-ECOINVENT_USERNAME = "ICL"
-ECOINVENT_PASSWORD = "ICL_2019-20"
